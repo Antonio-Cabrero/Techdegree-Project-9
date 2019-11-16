a88c6954227636a25bf3d659668ce43206b1e91b
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-11-16 22:09:00 +0100

    Added 'react-spring' to help transition between the page components

--- portfolio-site/src/pages/index.js
+++ portfolio-site/src/pages/index.js
@@ -1,30 +1,45 @@
 import React, { useState } from "react"
 import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import SEO from "../components/seo"
 import About from "./about"
 import Skills from './skills'
 import Projects from './projects'
 import Contact from './contact'
+import { useTransition, animated } from "react-spring"
 
 const IndexPage = () => {
 
-  const [content, setContent] = useState(<About />)
+  const [contentIndex, setContentIndex] = useState(0)
+  const contentArr = [
+     ({style}) => <animated.div style={{...style}}> <About /> </animated.div>,
+     ({style}) => <animated.div style={{...style}}> <Skills /> </animated.div>, 
+     ({style}) => <animated.div style={{...style}}> <Projects /> </animated.div>,
+     ({style}) => <animated.div style={{...style}}>  <Contact /> </animated.div>,
+  ]
 
   const onNavbarClick = (button) => {
 
     const target = button.target.innerText
     switch (target) {
-      case "ABOUT" : return setContent(<About />)
-      case "SKILLS": return setContent(<Skills />)
-      case "PROJECTS": return setContent(<Projects />)
-      case "CONTACT": return setContent(<Contact />)
+      case "ABOUT" :  return setContentIndex(0)
+      case "SKILLS":  return setContentIndex(1)
+      case "PROJECTS": return setContentIndex(2)
+      case "CONTACT": return setContentIndex(3)
     }
   }
+  const transition = useTransition(contentIndex , p => p, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-100%,0,0)' },
+  })
 
   return ( 
   <Layout handleMenu={onNavbarClick}>
-    <SEO title="home" />
-    {content}
+    <SEO title="home" />      
+    {transition.map(({item, props , key}) => {
+      const Page = contentArr[item]
+      return <Page key={key} style={props} />
+    })}
   </Layout>
   )
 }
