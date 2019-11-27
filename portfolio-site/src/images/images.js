7c481ff35bf0212ba9ec9f6f28a7daa34b06249d
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-11-27 11:34:44 +0100

    Updates

--- portfolio-site/src/images/images.js
+++
@@ -1,8 +0,0 @@
-const images = [
-    {id: "pomodoro", src: "./img-pomodoro.JPG"},
-    {id: "webApp", src: "./web-dashboard.JPG"},
-    {id: "gallery", src: "./img-gallery.JPG"},
-    {id: "", src: ""},
-]
-
-export default images
